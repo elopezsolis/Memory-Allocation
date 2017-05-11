484e8604078de89950053dc7b5bc01dc8e1794e7
Erick <elopezsolis@cpp.edu>
2017-05-11 12:25:09 -0700

    Added class Job that handles a job

---
+++ Job.java
@@ -0,0 +1,23 @@
+/**
+ * A job or process in this project will contain two things:
+ * - pid
+ * - size
+ */
+public final class Job {
+    private final int pid;
+    private final int size;
+    public Job(int pid, int size) {
+        this.pid = pid;
+        this.size = size;
+    }
+    public int getPid() {
+        return pid;
+    }
+    public int getSize() {
+        return size;
+    }
+    @Override
+    public String toString() {
+        return String.format("[%d %d]", pid, size);
+    }
+}
